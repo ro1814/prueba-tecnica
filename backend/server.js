5a76119b89b0348ea2ec18ec6e754d52bc5e6daa
Rodolfo S <rnsm14@gmail.com>
2021-03-07 22:08:09 +0100

    final push

--- backend/server.js
+++ backend/server.js
@@ -4,22 +4,29 @@ import connectDB from './config/db.js'
 import cors from 'cors'
 import fabRoutes from './routes/fabRoutes.js'
 import productRoutes from './routes/productRoutes.js'
+import colors from 'colors'
+import path from 'path'
 
 //Cuando se agregan archivos (de cualquier tipo) seguir el ejemplo: import products from './data/products.js'
-
+const __dirname = path.resolve()
 dotenv.config();
 
 connectDB()
 
 const app = express()
 
-app.get('/', (req, res) => {
-    res.send('API is running...')
-})
 
 app.use('/api/products', productRoutes)
 app.use('/api/fabs', fabRoutes)
 
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, '/frontend/build')))
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
+} else {
+    app.get('/', (req, res) => {
+        res.send('API is running...')
+    })
+}
 
 app.use(cors())
 
